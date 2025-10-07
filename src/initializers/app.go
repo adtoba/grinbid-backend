6941d3e5fd2879cb0ab475c9e00537499d74e014
adtoba <adetoba53@gmail.com>
2025-10-07 12:02:57 +0100

    init: Read env into config

---
+++ src/initializers/app.go
@@ -0,0 +1,27 @@
+package initializers
+
+import "github.com/spf13/viper"
+
+type Config struct {
+	DBUsername string `mapstructure:"POSTGRES_USER"`
+	DBPassword string `mapstructure:"POSTGRES_PASSWORD"`
+	DBHost     string `mapstructure:"POSTGRES_HOST"`
+	DBPort     string `mapstructure:"POSTGRES_PORT"`
+	DBName     string `mapstructure:"POSTGRES_DB"`
+}
+
+func LoadConfig(path string) (config Config, err error) {
+	viper.AddConfigPath(path)
+	viper.SetConfigName("app")
+	viper.SetConfigType("env")
+
+	viper.AutomaticEnv()
+
+	err = viper.ReadInConfig()
+	if err != nil {
+		return
+	}
+
+	err = viper.Unmarshal(&config)
+	return
+}
